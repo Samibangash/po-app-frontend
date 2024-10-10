3ac6ec008f695d4b3f45695f08eaa3f8129c40d3
Asad ali <asadaliInovabridage>
2024-10-11 04:34:02 +0500

    up

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  loginForm!: FormGroup;
+
+  constructor(private fb: FormBuilder, private api: ApiService) { }
+
+  createForm() {
+    this.loginForm = this.fb.group({
+      username: [''],
+      password: ['']
+    });
+  }
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const formData = this.loginForm.value;
+      console.log('Form Data:', formData);
+      // Handle login logic here (e.g., call an API)
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 
 }
