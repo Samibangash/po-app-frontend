46510cc212ee2f8f2f42a860ed1acd8e188b97cb
SamiBangash <rehmansami16@hotmail.com>
2024-10-11 03:13:18 +0300

    cahnge

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -5,28 +5,26 @@ import { ApiService } from 'src/app/services/api.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
 
-  constructor(private fb: FormBuilder, private api: ApiService) { }
+  constructor(private fb: FormBuilder, private api: ApiService) {}
 
   ngOnInit(): void {
     this.createForm();
   }
   createForm() {
     this.loginForm = this.fb.group({
-      email: [''],
-      password: ['']
+      username: [''],
+      password: [''],
     });
   }
 
   onSubmit() {
-    this.api.login(this.loginForm.value).subscribe(response=>{
+    this.api.login(this.loginForm.value).subscribe((response) => {
       console.log(response);
-      
     });
   }
-
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,19 +1,46 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-  baseURL = "http://localhost:8080/";
+  baseURL = 'http://localhost:8080/api/';
+
+  // Create a common method to add headers
+  private getHttpOptions() {
+    return {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+    };
+  }
+
+  // Login method with headers
   login(data: any) {
-    return this.http.post(this.baseURL + "auth/login", data)
+    return this.http
+      .post(this.baseURL + 'auth/login', data, this.getHttpOptions())
+      .pipe(
+        catchError((error) => {
+          console.error('Login API error:', error);
+          return throwError(error);
+        })
+      );
   }
 
+  // Register method with headers
   register(data: any) {
-    return this.http.post(this.baseURL + "auth/login", data)
+    return this.http
+      .post(this.baseURL + 'auth/register', data, this.getHttpOptions())
+      .pipe(
+        catchError((error) => {
+          console.error('Register API error:', error);
+          return throwError(error);
+        })
+      );
   }
-
 }
